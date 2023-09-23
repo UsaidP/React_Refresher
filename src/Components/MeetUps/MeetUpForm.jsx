d0d51fb268828ff45e8cdb9521e3ab794a2c2ddc
Usaid Patel <Usaidpatel150@gmail.com>
2023-09-23 22:41:00 +0530

    post request with firebase

--- src/Components/MeetUps/MeetUpForm.jsx
+++ src/Components/MeetUps/MeetUpForm.jsx
@@ -1,30 +1,60 @@
-import React, { useRef } from "react";
-import Card from "../ui/Card";
-import classes from "./MeetUpForm.module.css";
+import { useRef } from 'react';
+import React from 'react';
+import Card from '../ui/Card';
+import classes from './MeetUpForm.module.css';
 
-function formHandler(event) {
-  const title = useRef();
-}
+function MeetUpForm(props) {
+  //For Reading values we mostly use useRef
+  const titleInputRef = useRef();
+  const imageInputRef = useRef();
+  const addressInputRef = useRef();
+  const descriptionInputRef = useRef();
 
-function MeetUpForm() {
+  function submitHandler(event) {
+    event.preventDefault();
+
+    //we are storing data which is user are typing in.eg:title etc, we are using .
+    //current property which use in JS for holding the current information entered by user.
+    //All the JS object has a value property which hold the current value of that input or other properties.
+    const enteredTitle = titleInputRef.current.value;
+    const enteredImage = imageInputRef.current.value;
+    const enteredAddress = addressInputRef.current.value;
+    const enteredDescription = descriptionInputRef.current.value;
+
+    const meetupData = {
+      title: enteredTitle,
+      image: enteredImage,
+      address: enteredAddress,
+      description: enteredDescription,
+    };
+    props.onAddMeetup(meetupData);
+  }
+
   return (
-    <Card className={classes.card}>
-      <form className={classes.form} onSubmit={formHandler}>
+    <Card>
+      <form className={classes.form} onSubmit={submitHandler}>
         <div className={classes.control}>
           <label htmlFor='title'>Meetup Title</label>
-          <input type='text' required id='title' />
+          {/* ref is React built-in key word that is use to make connection between HTML element and useRef() with this we can access that element data and use them as we want.*/}
+          <input type='text' required id='title' ref={titleInputRef} />
         </div>
         <div className={classes.control}>
           <label htmlFor='image'>Meetup Image</label>
-          <input type='url' required id='image' />
+          <input type='url' required id='image' ref={imageInputRef} />
         </div>
         <div className={classes.control}>
           <label htmlFor='address'>Meetup Address</label>
-          <input type='text' required id='address' />
+          <input type='text' required id='address' ref={addressInputRef} />
         </div>
         <div className={classes.control}>
           <label htmlFor='description'>Meetup Description</label>
-          <textarea type='text' required id='description' rows='3' />
+          <textarea
+            type='text'
+            required
+            id='description'
+            rows='3'
+            ref={descriptionInputRef}
+          />
         </div>
         <div className={classes.actions}>
           <button> Add Meetup</button>
@@ -33,5 +63,4 @@ function MeetUpForm() {
     </Card>
   );
 }
-
 export default MeetUpForm;
--- src/pages/NewMeetUps.jsx
+++ src/pages/NewMeetUps.jsx
@@ -1,10 +1,22 @@
-import React from "react";
-import MeetUpForm from "../Components/MeetUps/MeetUpForm";
+import React from 'react';
+import MeetUpForm from '../Components/MeetUps/MeetUpForm';
 function NewMeetUps() {
+  function handlerMeetupData(meetupData) {
+    fetch(
+      'https://reactrefrasher-default-rtdb.firebaseio.com/meetupData.json',
+      {
+        method: 'POST',
+        body: JSON.stringify(meetupData),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    );
+  }
   return (
     <div>
       <h2>New MeetUps</h2>
-      <MeetUpForm />
+      <MeetUpForm onAddMeetup={handlerMeetupData} />
     </div>
   );
 }
